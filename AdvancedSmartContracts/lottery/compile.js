ec565c0aff6ca498c0668dc0429c2427a0d89f80
iamarchit2 <architaggarwal0508@gmail.com>
2019-04-10 22:44:43 +0530

    Modified

---
+++ AdvancedSmartContracts/lottery/compile.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc')
+// Cannot directly require .sol Files.
+// Thus, we create a path and access it.
+
+const lotteryPath = path.resolve(__dirname, 'contracts', 'lottery.sol');
+// __dirname takes you to the working directory directly.
+const source = fs.readFileSync(lotteryPath, 'utf8');
+// read the file through fs -> FileSync
+module.exports = solc.compile(source, 1).contracts[':Lottery'];
---
+++ AdvancedSmartContracts/lottery/contracts/lottery.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.17;
+contract Lottery {
+    address public manager; //public so that javascript program can access it.
+    address[] public players;
+    function Lottery() public {
+        manager = msg.sender;
+    }
+    function getPlayers() public view returns(address[]){
+        return players;
+    }
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
+    }
+    function random() private view returns (uint){
+       return uint(keccak256(block.difficulty, now, players));
+    }
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);
+        players = new address[](0);
+    }
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+}
