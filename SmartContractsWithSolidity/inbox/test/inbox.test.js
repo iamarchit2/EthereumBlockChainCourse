95ef84c693225805b680046328b60d0ab90c1138
iamarchit2 <architaggarwal0508@gmail.com>
2019-04-10 13:19:53 +0530

    Modified

--- SmartContractsWithSolidity/inbox/test/inbox.test.js
+++ SmartContractsWithSolidity/inbox/test/inbox.test.js
@@ -2,7 +2,8 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 // Constructor Function, creats an Instance.
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const {interface, bytecode} = require('../compile');
 // Testing.
 /*class Car {
@@ -37,9 +38,19 @@ beforeEach(async () => {
    inbox = await new web3.eth.Contract(JSON.parse(interface))
    .deploy({ data: bytecode, arguments: ['Archit'] })
    .send({ from: accounts[0], gas: '1000000' });
+   inbox.setProvider(provider)
 });
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address); //Makes sure that the arguement is a defined value.
+  });
+  it('has a default message', async () =>{
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Archit');
+  });
+  it('can change a message', async () => {
+    await inbox.methods.setMessage('bye').send({ from : accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
   });
 });
